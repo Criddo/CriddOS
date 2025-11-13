dca1c0adc0c9ca2f555b3d652834f18ff9b0468d
Chris <chrismdixon91@gmail.com>
2025-11-14 07:28:11 +1000

    Delete boot_sect_simple.asm

--- boot_sect_simple.asm
+++
@@ -1,8 +0,0 @@
-; Creates infinite loop
-loop:
-    jmp loop
-
-; Fill with 510 zeros minus the size of the previous code
-times 510-($-$$) db 0
-; Ensuring bytes 511 and 512 trigger bootable media
-dw 0xaa55
